68792e6529e264302b12d9efb69a87f4d90042a4
bs <23002942+berksudan@users.noreply.github.com>
2022-05-13 01:31:32 +0200

    first data files added

--- src/initial_python_file.py
+++ src/initial_python_file.py
@@ -1,2 +1,5 @@
-# It is a placeholder
+import os
+from bertopic import BERTopic
+from sklearn.datasets import fetch_20newsgroups
 
+docs = fetch_20newsgroups(subset="all", remove=("headers", "footers", "quotes"),data_home=DATA_FOLDER)["data"]
