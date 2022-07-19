8b87905af27a10ece16ef50230c5c70611cc70e8
bs <23002942+berksudan@users.noreply.github.com>
2022-07-19 22:45:32 +0200

    LDA_NMF_runner tested

--- src/LDA_NMF_runner.py
+++ src/LDA_NMF_runner.py
@@ -18,107 +18,111 @@ from octis.dataset.dataset import Dataset
 from octis.evaluation_metrics.diversity_metrics import TopicDiversity
 from octis.evaluation_metrics.coherence_metrics import Coherence
 
+from gensim.corpora import Dictionary
+
 from src import visualizer
 
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
+def df_to_vocab_old(df):
+    import itertools
+    word_list_of_lists = [word_tokenize(df.Text[i]) for i in range(df.shape[0]) ]
+    return list(set(list(itertools.chain.from_iterable(word_list_of_lists))))
 
+# The following three functions are used to make vocabulary.txt
 
-# Please choose the entries from the following sets:
-    
-# model: ['LDA', 'NMF']
-# dataset: ['crisis_12', '20news']
+def make_word_list(documents):
+    dataset = []
+    for text in documents:
+        tokens = nltk.word_tokenize(text)
+        dataset.append(tokens)
+    vocab = Dictionary(documents=dataset, prune_at=20000)
+    vocab.filter_extremes(no_below=1, no_above=1, keep_n=2000)
+    word_list = []
+    for key in vocab.token2id.keys():
+        if len(key) > 1 or key == 'a':
+            word_list.append(key)
+    return word_list 
 
-# Other parameters
-# top_n_topics: int (This applies only when vis_labels_per_topic is True, it decides how many topics are shown)
-# n_words: int ((This applies only when vis_barchart is True, it decides how many words are shown))
+def make_new_documents(documents, word_list):
+    for i in range(len(documents)):
+        new_sen = []
+        for word in nltk.word_tokenize(documents[i]):
+            if word in word_list:
+                new_sen.append(word)
+        documents[i] = ' '.join(new_sen).strip()
+    return documents
 
-# Default setting, you can set your own parameters in the notebook
-settings_example = {'model': 'LDA',
-                    'dataset': 'crisis_12',
-                    'top_n_topics': 5,
-                    'n_words': 5}
-
-valid_models = ['LDA', 'NMF']
-valid_dataset = ['crisis_12', '20news']
-
-#TODO: add a dir here, maybe using a class is easier
-save_dir_crisis_12 = '/content/drive/MyDrive/SS_2022_Praktikum/Crisis Dataset/Dataset_12'
+def list_to_vocab(word_list, save_dir):
+    txt = open(f"{save_dir}/vocabulary.txt", 'w')
+    for i in range(len(word_list)):
+        txt.write(word_list[i])
+        txt.write('\r\n')
+    txt.close()
 
 #TODO: solve random state problem (we may need multiple runs to get average)
-def runner(model_name, dataset_name, top_n_topics,
-           n_words, save_dir, random_state = 98):
-    
-    # Make some assertions
-    if model_name not in valid_models:
-        raise ValueError(f'{model_name} not in valid embedding models: {valid_models}.')
+def runner(args):
 
-    if dataset_name not in valid_dataset:
-        raise ValueError(f'{dataset_name} not in valid embedding models: {valid_dataset}.')
+    word_list = make_word_list(args['docs'])
+    documents = make_new_documents(args['docs'], word_list)
+    list_to_vocab(word_list, args['data_save_dir'])
     
-    
-    # Define dataset
+    # Fit the data into OCTIS format, so that OCTIS can read it
+    col_names = ('Text', 'Partition', 'Topics')
+    result_all = []
+
+    for i in range(len(args['docs'])):
+        if len(args['docs'][i].split()) >= 3:     # Delete data that cannot be recognized by OCTIS
+            result = []
+            result.append(documents[i])
+            result.append('train')
+            result.append(args['labels'][i])
+            result_all.append(result)
+
+    df_dataset = pd.DataFrame(result_all, columns = col_names)
+    df_dataset = df_dataset.sample(frac=1.0, random_state = 100).reset_index(drop=True)
+    df_dataset.to_csv(f"{args['data_save_dir']}/corpus.tsv",\
+                      sep = '\t', index=False, header = None)
+    df_dataset.to_csv(f"{args['data_save_dir']}/corpus_with_header.tsv",\
+                      sep = '\t', index=False)
+
+    # The data is now ready, let's start training!
     dataset = Dataset()
-    if dataset_name == 'crisis_12':
-        dataset.load_custom_dataset_from_folder(save_dir)
-        num_topics = 4
-    elif dataset_name == '20news':
-        dataset.fetch_dataset("20NewsGroup")
-        num_topics = 20
+    dataset.load_custom_dataset_from_folder(args['data_save_dir'])
 
     # Document start time and end time to get the time the model has used    
     start_time = time.time()
 
-    if model_name == 'LDA':
-        OCTIS_model = LDA(num_topics = num_topics, random_state = random_state)        # Create the model
-        output = OCTIS_model.train_model(dataset)                                      # Train the model
+    if args['model_name'] == 'lda':
+        OCTIS_model = LDA(num_topics = args['num_topics'], random_state = args['random_state'])        # Create the model
+        output = OCTIS_model.train_model(dataset)                                                      # Train the model
 
-    elif model_name == 'NMF':
-        OCTIS_model = NMF(num_topics = num_topics, random_state = random_state)        # Create the model
-        output = OCTIS_model.train_model(dataset)                                      # Train the model
+    elif args['model_name'] == 'nmf':
+        OCTIS_model = NMF(num_topics = args['num_topics'], random_state = args['random_state'])        # Create the model
+        output = OCTIS_model.train_model(dataset)                                                      # Train the model
     
     end_time = time.time()
     run_time = round(end_time-start_time, 2)
-
-    # Initialize topic coherence metric
-    npmi = Coherence(texts=dataset.get_corpus(), topk=10, measure='c_v')
-    # Initialize topic diversity metric
-    topic_diversity = TopicDiversity(topk=10)
-    
-    # Get the TC and TD scores
-    npmi_score = npmi.score(output)
-    topic_diversity_score = topic_diversity.score(output)
-    
-    # To match the real label of crisis_12, we need a dict
-    #TODO: maybe we can delete it
-    in_labels_crisis_12 = {0:'earthquake',
-                           1:'floods',
-                           2:'forestfires',
-                           3:'hurricanes'}
     
     #TODO: fit this to 20news, currently only crisis_12
     # Get Doc-Topic Output
     col_names_DT = ('run_id', 'Document ID', 'Document', 'Real Label', 'Assigned Topic Num', 'Assignment Score')
     result_DT = []
         
-    # Dataset 12
-    df = pd.read_csv('/content/drive/MyDrive/SS_2022_Praktikum/Crisis Dataset/Dataset_12/corpus_with_header.tsv', delimiter='\t')
+    df = pd.read_csv(args['data_save_dir'] + '/corpus_with_header.tsv', delimiter='\t')
         
     # Crisis dataset
-    df = df.loc[df['partition'] == 'train']
+    df = df.loc[df['Partition'] == 'train']
     output_max = np.max(output['topic-document-matrix'], axis = 0)
     output_index = np.argmax(output['topic-document-matrix'], axis = 0)
+    #print(output_index)
     length = len(output_max)
         
     for i in range(length):
         result_DT_line = []
         
-        result_DT_line.append(start_time)
+        result_DT_line.append(int(start_time))
         result_DT_line.append(i)
-        result_DT_line.append(df[i:i+1].Tweets.values[0])
-        result_DT_line.append(in_labels_crisis_12[df[i:i+1].Topics.values[0]])
+        result_DT_line.append(df[i:i+1].Text.values[0])
+        result_DT_line.append(df[i:i+1].Topics.values[0])
         result_DT_line.append(output_index[i])
         result_DT_line.append(output_max[i])
         result_DT.append(result_DT_line)
@@ -138,19 +142,20 @@ def runner(model_name, dataset_name, top_n_topics,
     scores = output['topic-word-matrix']
         
     word_scores_all = []
-    for i in range(4):
+    for i in range(args['num_topics']):
         scores[i].sort()
         word_scores_line = scores[i][-10:][::-1]
+        word_scores_line /= np.sum(word_scores_line)
         word_scores_all.append(word_scores_line)
         
-    for i in range(4):
+    for i in range(args['num_topics']):
         result_TW_line = []
         
-        result_TW_line.append(start_time)
-        result_TW_line.append(model_name)
+        result_TW_line.append(int(start_time))
+        result_TW_line.append(args['model_name'])
         result_TW_line.append('None')
-        result_TW_line.append(dataset_name)
-        result_TW_line.append(4)
+        result_TW_line.append(args['data_name'])
+        result_TW_line.append(args['num_topics'])
         result_TW_line.append('None')
         result_TW_line.append(i)
         result_TW_line.append(size_count[i])
@@ -165,19 +170,14 @@ def runner(model_name, dataset_name, top_n_topics,
 
     return Doc_Topic_df, Topic_word_df
 
-
-
-#TODO: Add TC and TD output
-#TODO: Add all_run (4 combinations for now)
-
-
-if __name__ == '__main__':
-    Doc_Topic_df, Topic_word_df = runner(model_name = settings_example['model'], 
-                                         dataset_name = settings_example['dataset'], 
-                                         top_n_topics = settings_example['top_n_topics'], 
-                                         n_words = settings_example['n_words'],
-                                         save_dir = save_dir_crisis_12,
-                                         random_state = 100)
+# TODO: Do we need that?
+#if __name__ == '__main__':
+#    Doc_Topic_df, Topic_word_df = runner(model_name = settings_example['model'], 
+#                                         dataset_name = settings_example['dataset'], 
+#                                         top_n_topics = settings_example['top_n_topics'], 
+#                                         n_words = settings_example['n_words'],
+#                                         save_dir = save_dir_crisis_12,
+ #                                        random_state = 100)
 
 
 
